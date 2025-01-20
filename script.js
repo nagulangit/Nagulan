45f6b1da6ed73b18f24958e3d6357c5b7f42e8a8
nagulangit <nagulanayyasamy226@gmail.com>
2025-01-20 18:49:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 particlesJS('particles-js', {
     "particles": {
         "number": {
-            "value": 100,
+            "value": 80,
             "density": {
                 "enable": true,
                 "value_area": 800
@@ -24,43 +24,16 @@ particlesJS('particles-js', {
             }
         },
         "opacity": {
-            "value": 0.6,
+            "value": 0.5,
             "random": true,
             "anim": {
                 "enable": true,
                 "speed": 1,
-                "opacity_min": 0
-            }
-        },
-        "line_linked": {
-            "enable": true,
-            "distance": 150,
-            "color": "#ff6f61",
-            "opacity": 0.4,
-            "width": 1
-        },
-        "move": {
-            "enable": true,
-            "speed": 3,
-            "direction": "none",
-            "random": false,
-            "straight": false,
-            "out_mode": "out",
-            "bounce": false
-        }
-    },
-    "interactivity": {
-        "detect_on": "window",
-        "events": {
-            "onhover": {
-                "enable": true,
-                "mode": "repulse"
-            },
-            "onclick": {
-                "enable": true,
-                "mode": "push"
+                "opacity_min": 0.1
             }
         }
-    },
-    "retina_detect": true
+    }
 });
+
+// AOS (Animate on Scroll)
+AOS.init();
