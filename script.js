5cc3c8a47193b582f042e46e7ea318e5737e312f
nagulangit <nagulanayyasamy226@gmail.com>
2025-01-20 18:36:02 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,39 @@
+// Initialize particles.js for fun, dynamic background
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 100,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ff00ff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#ff00ff"
+      },
+      "polygon": {
+        "nb_sides": 5
+      }
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 6,
+        "size_min": 0.3,
+        "sync": false
+      }
+    },
+    "move": {
+      "enable": true,
+      "speed": 3,
+      "direction": "none",
+      "random": false,
+      "straight":
