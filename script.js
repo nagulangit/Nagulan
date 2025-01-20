be18c4b86ca855c73466a0060a47d4b6184cbd52
nagulangit <nagulanayyasamy226@gmail.com>
2025-01-20 18:42:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-// Initialize particles.js for fun, dynamic background
-particlesJS("particles-js", {
+// Play a sound on button click
+function playClickSound() {
+    const clickSound = new Audio('click-sound.mp3');
+    clickSound.play();
+}
+
+// Particles.js for fun background effects
+particlesJS('particles-js', {
   "particles": {
     "number": {
       "value": 100,
@@ -15,7 +21,7 @@ particlesJS("particles-js", {
       "type": "circle",
       "stroke": {
         "width": 0,
-        "color": "#ff00ff"
+        "color": "#000000"
       },
       "polygon": {
         "nb_sides": 5
@@ -26,14 +32,48 @@ particlesJS("particles-js", {
       "random": true,
       "anim": {
         "enable": true,
-        "speed": 6,
-        "size_min": 0.3,
-        "sync": false
+        "speed": 4,
+        "size_min": 0.3
       }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 1,
+        "opacity_min": 0
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ff00ff",
+      "opacity": 0.4,
+      "width": 1
     },
     "move": {
       "enable": true,
       "speed": 3,
       "direction": "none",
       "random": false,
-      "straight":
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false
+    }
+  },
+  "interactivity": {
+    "detect_on": "window",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      }
+    }
+  },
+  "retina_detect": true
+});
